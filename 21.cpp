049cecbbc5949a8b9f9512854a6401f52d3a9b2e
Harshu8 <69030020+Harshu8@users.noreply.github.com>
2022-07-15 22:47:59 +0530

    Leetcode: 237. Delete Node in a Linked List

---
+++ 21.cpp
@@ -0,0 +1,12 @@
+// Leetcode: 237. Delete Node in a Linked List
+// link: https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        
+        node -> val = node -> next -> val;
+        node -> next = node -> next -> next;
+        return;
+    }
+};
