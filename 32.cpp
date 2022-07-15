48bb8967bbee1dce491200d24dae0d1fe44bba69
Harshu8 <69030020+Harshu8@users.noreply.github.com>
2022-07-15 23:01:40 +0530

    GFG: Delete without head pointer

---
+++ 32.cpp
@@ -0,0 +1,18 @@
+// GFG: Delete without head pointer 
+// link: https://practice.geeksforgeeks.org/problems/delete-without-head-pointer/1/?page=1&difficulty[]=0&status[]=unsolved&category[]=Linked%20List&sortBy=submissions#
+
+
+
+class Solution
+{
+    public:
+    //Function to delete a node without any reference to head pointer.
+    void deleteNode(Node *del)
+    {
+       // Your code here
+       del -> data = del -> next -> data;
+       del -> next = del -> next -> next;
+       return;
+    }
+
+};
